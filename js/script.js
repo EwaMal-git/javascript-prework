644dc72c5fe1a5cca1af68ba2f0986b8fbd90478
Ewa Malerek <ewa.malerek1985@gmail.com>
2021-05-31 11:34:09 +0200

    Add code java-script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-var playerName;
-playerName = 'papier';
-printMessage('Zagrałem ' + playerName + '! Jeśli Twój ruch to nożyce to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'Nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+'papier';
