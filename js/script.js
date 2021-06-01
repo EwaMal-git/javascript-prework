711fd1528590590ef7f736b94b267620d1724c6a
Ewa Malerek <ewa.malerek1985@gmail.com>
2021-06-01 08:36:08 +0200

    Add PlayerMove

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ if (randomNumber == '1') {
 printMessage('Mój ruch: ' + computerMove);
 
 'papier';
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+  printMessage('Twój ruch: ' + playerMove);
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+  printMessage('Twój ruch: ' + playerMove);
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+  printMessage('Twój ruch: ' + playerMove);
+} else {
+  playerMove = 'kamień';
+  printMessage(('Zły wybór !'));
+}
