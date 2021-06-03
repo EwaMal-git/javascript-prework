9ad267e64a99cf6323079cdcf16dfe3f7b16e72b
Ewa Malerek <ewa.malerek1985@gmail.com>
2021-06-03 18:08:35 +0200

    Change type of var in JS

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@
 const buttonRock = document.getElementById('button-rock'),
       buttonPaper = document.getElementById('button-paper'),
       buttonScissors = document.getElementById('button-scissors');
-
-let counterPlayer = document.getElementById('counter-player'),
-    counterComputer = document.getElementById('counter-computer'),
-    counterPl = 0,
-    counterCom = 0;
+      counterPlayer = document.getElementById('counter-player'),
+      counterComputer = document.getElementById('counter-computer');
+let   counterPl = 0,
+      counterCom = 0;
 
 function buttonClicked (argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
- 	let playerMove = argButtonName;
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
+ 	const playerMove = argButtonName;
+	const randomNumber = Math.floor(Math.random() * 3 + 1);
 	console.log('wylosowana liczba to: ' + randomNumber);
-	let computerMove = getMoveName(randomNumber);
+	const computerMove = getMoveName(randomNumber);
 	console.log('ruch komputera to: ' + computerMove);
 	displayResult(playerMove, computerMove);
 }
